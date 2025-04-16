75547767635d4312cb01a06506802759dcc541b3
RD-D-07 <rd710529@gmail.com>
2025-04-16 20:25:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let fname;
+let lname;
+let title;
+    document.getElementById('submit').onclick = function(){
+    fname = document.getElementById('fname').value;
+    lname = document.getElementById('lname').value;
+    title = document.getElementById('title').value;
+    document.getElementById('formtext').textContent = `Hey ${fname}, heres a small little website dedicaded to only you MY unbelievably hot and precious ${title}. i really hope you like it baby and the goal of me making this small little project of mine was so i can remind you just how special you are to me Mrs. ${lname} no matter where you are. Now you have what i aim to be a cute and gentle reminder of just how much you mean to me aswell as how much this beautiful and precious relationship we've built together means to me my love. Thank you for being the best ${title} i could ever ask for and for always being there for me my love and i want you to always remember that i will never forver how much you do for me and for us and i appreciate it so so much baby. I LOVE YOU FOREVER!! `   }
+ 
